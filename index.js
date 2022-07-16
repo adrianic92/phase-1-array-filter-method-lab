ce537a5ea597660b1b27bb80e70bd258613af281
Adrian <adrianic92@yahoo.com>
2022-07-16 10:06:02 -0700

    commit tree 7cb0e757f969de4c9b245f240e6afcfe419c530c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+// findMatching- This function takes an array of drivers' names and a string as arguments, and returns the matching list of drivers. The function should be case insensitive.
+
+function findMatching(driverArray, string) {
+    const match = driverArray.filter(function (words) { return words.toLowerCase() === string.toLowerCase()});
+    return match;
+}
+
+function fuzzyMatch(driverArray, string) {
+    const fuzzy = driverArray.filter(function (words) { return words.toLowerCase().indexOf(string.toLowerCase()) === 0 });
+    return fuzzy;
+}
+
+function matchName(driverArray, string) {
+    const property = driverArray.filter(function (words) {return words.name === string});
+    return property;
+}
